4fafeef9e6cb0274faf502b2ced299c766dc8737
picia2014 <pipi@onetka.pl>
2022-06-18 02:47:17 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	mainMethodsSentimentAnalysis.py
    #	wordOccurrences.xlsx

---
+++ Crawler/crawlerNew.py
@@ -0,0 +1,3 @@
+## This is going to be my new attempt of a crawler
+## The current one tries to go around forum defences but not always succeeds
+## so one again, perhaps this time I will figure out how to do it
--- main.py
+++ main.py
@@ -16,16 +16,16 @@ if __name__ == '__main__':
 
 
     ##create a list of subdirectories with which we will work
-    # fw = findWords.findWords(listOfWords, mainCatalog)
-    # catalogs = fw.listSubdirectories()
+    fw = findWords.findWords(listOfWords, mainCatalog)
+    catalogs = fw.listSubdirectories()
 
     ##iterate over every catalog and count word instances
     ##every catalog is another company
-    # ms = mainMethodsSentimentAnalysis.mainMethodsSentimentAnalysis(mainCatalog, listOfWords)
-    # wordOccurrences = ms.countWordOccurrences()
+    ms = mainMethodsSentimentAnalysis.mainMethodsSentimentAnalysis(mainCatalog, listOfWords)
+    wordOccurrences = ms.countWordOccurrences()
 
     ## analyze sentiment of joined texts on every company
-    # ms.runSentimentAnalysisForEveryCompany()
+    ms.runSentimentAnalysisForEveryCompany()
 
 
 
